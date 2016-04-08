1eda5d76d888f5e76c561e9c2d02b3f2f806ac34
DejanMladjenovic <dejo.muscar@gmail.com>
2016-04-08 21:29:41 +0200

    Update Instrukcije.java

--- src/konverter/Instrukcije.java
+++ src/konverter/Instrukcije.java
@@ -15,9 +15,9 @@ public class Instrukcije {
 	}
 
 	// Metoda cUi
-	public static void centimetreUinche(double a, double b) {
+	public static void centimetreUinche(double a) {
 		double inc = a / 0.393700787;
-		System.out.printf("%.2f centimetara je %.2f inèa.\n", a, inc);
+		System.out.printf("%.2f centimetara je %.2f inÃ¨a.\n", a, inc);
 	}
 
 	// Metoda iUc
