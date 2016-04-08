a9c80b66d78f0cf6962c000e44cce500529757d9
Ana Jozic Agic <ana.jozic.agic@kolektiv.eu>
2016-04-08 10:23:49 +0200

    Initial commit

---
+++ src/konverter/Instrukcije.java
@@ -0,0 +1,8 @@
+package konverter;
+
+public class Instrukcije {
+public static double kilogrameUfunte(double a, double b){
+	double rezultat=0;
+	return rezultat;
+}
+}
---
+++ src/konverter/KonverterApp.java
@@ -0,0 +1,13 @@
+package konverter;
+
+public class KonverterApp {
+
+	public static void main(String[] args) {
+		double a,b;
+		a=0;
+		b=0;
+		System.out.println(Instrukcije.kilogrameUfunte(a, b));
+		
+	}
+
+}
