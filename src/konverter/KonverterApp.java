e169071fcc6da4ac024f4f13a4a32755f9387c4d
Ana Jozic Agic <ana.jozic.agic@kolektiv.eu>
2016-04-08 10:49:18 +0200

    Dodana metoda fuc

--- src/konverter/KonverterApp.java
+++ src/konverter/KonverterApp.java
@@ -3,11 +3,11 @@ package konverter;
 public class KonverterApp {
 
 	public static void main(String[] args) {
-		double a,b;
-		a=0;
-		b=0;
-		System.out.println(Instrukcije.kilogrameUfunte(a, b));
-		
+		double a, b;
+		a = 0;
+		b = 0;
+		Instrukcije.kilogrameUfunte(a, b);
+
 	}
 
 }
